dcd730790e3fd21f3195568220b1f52dc22ff3df
moh.afrizal <moh.afrizal@bni.co.id>
2024-04-13 09:47:49 +0700

    change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='coreai-sdk',
+    name='coreai_sdk',
     version='0.1.0',
     description='A simple Python package for cryptographic operations.',
     long_description=open('README.md').read(),
